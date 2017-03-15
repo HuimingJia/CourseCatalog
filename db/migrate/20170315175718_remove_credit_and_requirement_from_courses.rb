fb06b143b0d8f9dea4c2e19f20b99998e6452bfe
HuimingJia <jiahm92@gmail.com>
2017-03-15 14:13:07 -0400

    table shape

--- db/migrate/20170315175718_remove_credit_and_requirement_from_courses.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCreditAndRequirementFromCourses < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :courses, :credit, :string
-    remove_column :courses, :requirement, :string
-  end
-end
